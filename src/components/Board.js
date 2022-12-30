8f0d85d0fbc526071b250da18687174c021b25a7
Owahed Hossain <owahedhossain@gmail.com>
2022-12-30 21:49:50 +0600

    done

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,6 +3,7 @@ import Square from "./Square";
 import "../App.css";
 
 const Board = ({ squares, onClick }) => {
+  //hi
   return (
     <div className="board">
       {squares.map((square, i) => (
